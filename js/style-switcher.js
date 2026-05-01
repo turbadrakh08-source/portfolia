d8ffc0ab6779d4c8b45ddef12a94e30dafd41742
Turbadrakh <turbadrah612@gmail.com>
2026-05-02 00:37:02 +0200

    last change

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -26,7 +26,6 @@ const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click", () => {
   dayNight.querySelector("i").classList.toggle("fa-sun");
   dayNight.querySelector("i").classList.toggle("fa-moon");
-  document.body.classList.toggle("white");
 });
 window.addEventListener("load", () => {
   if (document.body.classList.contains("dark")) {
